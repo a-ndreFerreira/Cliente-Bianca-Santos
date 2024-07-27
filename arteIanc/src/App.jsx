62672770e9623e3486baa57ae35ca98bbc9f2a87
André Ferreira <tulibanet@hotmail.com>
2024-07-27 14:13:57 -0300

    Background dinamico + sleeve

--- arteIanc/src/App.jsx
+++ arteIanc/src/App.jsx
@@ -1,22 +1,52 @@
 
 import { Outlet } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 
 import './App.css'
 
 import Footer from './components/Footer/Footer'
 import Navbar from './components/Navbar/Navbar'
 
-import { img1, img2 } from './assets/bg_img/bgImg.js'
+import img1 from './assets/bg_img/clockFace.jpeg';
+import img2 from './assets/bg_img/double.jpeg';
+import img3 from './assets/bg_img/heart.jpeg';
 
-const images = [img1, img2];
 
 function App() {
+
+  const [background, setBackground] = useState(null);
+
+  useEffect(() => {
+
+    const images = [img1, img2, img3];
+
+    const changeBackground = () => {
+
+      const randomBackground = images[Math.floor(Math.random() * images.length)];
+      setBackground(`url(${randomBackground})`);
+
+    }
+
+    return changeBackground();
+
+  }, [])
 
   return (
     <div>
       <Navbar />
-      <div id='container'>
-        <Outlet />
+      <div id='container' style={{
+        position: 'relative',
+        backgroundImage: background,
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        backgroundPosition: 'center',
+        overflow: 'hidden'
+      }}>
+        <div id='sleeve'></div>
+
+        <div id='containerOutlet'>
+          <Outlet />
+        </div>
       </div>
       <Footer />
     </div>
--- arteIanc/src/assets/bg_img/bgImg.js
+++
@@ -1,5 +0,0 @@
-
-import img1 from './clockFace.jpeg';
-import img2 from './double.jpeg';
-
-export { img1, img2 };
