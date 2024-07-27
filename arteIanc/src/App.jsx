fe1e9a7f9c26d3325883c4e1e019a582038f19af
André Ferreira <tulibanet@hotmail.com>
2024-07-27 12:30:18 -0300

    Aplicação background dinamico

--- arteIanc/src/App.jsx
+++ arteIanc/src/App.jsx
@@ -6,6 +6,10 @@ import './App.css'
 import Footer from './components/Footer/Footer'
 import Navbar from './components/Navbar/Navbar'
 
+import { img1, img2 } from './assets/bg_img/bgImg.js'
+
+const images = [img1, img2];
+
 function App() {
 
   return (
---
+++ arteIanc/src/assets/bg_img/bgImg.js
@@ -0,0 +1,5 @@
+
+import img1 from './clockFace.jpeg';
+import img2 from './double.jpeg';
+
+export { img1, img2 };
